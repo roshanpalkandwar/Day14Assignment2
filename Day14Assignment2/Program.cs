d496b0b7f5a910e3a9ec67a6a1a1bb8ee5a07f0e
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-14 23:29:28 +0530

    [roshan]uc4

--- Day14Assignment2/Program.cs
+++ Day14Assignment2/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("chooes number\n1.Ability to create a Stack of\n2.Ability to peak and pop\n3.Ability to create a Queue");
+            Console.WriteLine("chooes number\n1.Ability to create a Stack of\n2.Ability to peak and pop\n3.Ability to create a Queue\n4.Ability to dequeue from the beginning");
             int opt=Convert.ToInt32(Console.ReadLine());   
             
             switch(opt)
@@ -32,6 +32,14 @@
                     queue1.Enqueue(30);
                     queue1.Enqueue(70);
                     queue1.Display();
+                    break;
+                    case 4:
+                    QueueDequeueOperation queue2 = new QueueDequeueOperation();
+                    queue2.Enqueue(56);
+                    queue2.Enqueue(30);
+                    queue2.Enqueue(70);
+                    queue2.IsEmpty();
+                    queue2.Display();
                     break;
             }
         }
