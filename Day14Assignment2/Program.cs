e61fa027eb547896fba23992e05da1410515891d
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-14 22:52:46 +0530

    [roshan]uc1

---
+++ Day14Assignment2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Day14Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chooes number\n1.Ability to create a Stack of");
+            int opt=Convert.ToInt32(Console.ReadLine());   
+            
+            switch(opt)
+            {
+                case 1:
+                    StackPushOperation stack1 = new StackPushOperation();
+                    stack1.Push(70);
+                    stack1.Push(30);
+                    stack1.Push(56);
+                    stack1.Display();
+                    break;
+            }
+        }
+    }
+}
